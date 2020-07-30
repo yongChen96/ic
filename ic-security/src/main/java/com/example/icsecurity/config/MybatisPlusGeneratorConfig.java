9b756181c8391252bc0baa7d15add7174c398f73
yongchen <cy19950906>
2020-07-30 18:03:18 +0800

    添加MyBatis-Plus-Generator代码生成依赖

---
+++ ic-security/src/main/java/com/example/icsecurity/config/MybatisPlusGeneratorConfig.java
@@ -0,0 +1,10 @@
+package com.example.icsecurity.config;
+
+/**
+ * @ClassName: MybatisPlusGeneratorConfig
+ * @Description: Mybatis-pius-generator代码生成
+ * @Author: yongchen
+ * @Date: 2020/7/30 17:42
+ **/
+public class MybatisPlusGeneratorConfig {
+}
