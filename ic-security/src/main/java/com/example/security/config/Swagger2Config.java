eb60a31d5b4db56bc3d78f9570bb01025d9d9080
黄忠益 <1353535080@qq.com>
2020-08-11 14:39:26 +0800

    完成security安全认证,以及登录日志和操作日志

--- ic-security/src/main/java/com/example/security/config/Swagger2Config.java
+++ ic-security/src/main/java/com/example/security/config/Swagger2Config.java
@@ -1,17 +1,24 @@
 package com.example.security.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName: Swagger2Config
@@ -21,8 +28,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  **/
 @Configuration
 @EnableSwagger2
-@EnableKnife4j
-@Import(BeanValidatorPluginsConfiguration.class)
+@EnableSwaggerBootstrapUI
 public class Swagger2Config {
     @Bean
     public Docket createRestApi() {
@@ -31,7 +37,8 @@ public class Swagger2Config {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.security.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(globalOperation());
     }
 
     private ApiInfo apiInfo() {
@@ -42,5 +49,16 @@ public class Swagger2Config {
                 .contact("developer@mail.com")
                 .version("v1.0")
                 .build();
+    }
+
+
+    private List<Parameter> globalOperation() {
+        //添加head参数配置start
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        //第一个token为传参的key，第二个token为swagger页面显示的值
+        tokenPar.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return pars;
     }
 }
